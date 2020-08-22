19c447ef04c889c265c1b7072a7f2d57b4e623a9
Mingye Wang <arthur2e5@aosc.io>
2020-08-22 13:36:27 +0800

    x86 (modern): add -msahf
    SAHF/LAHF makes a lot of flag stuff faster than PUSHF/POPF. This kills support for
    Pentium 4 pre-stepping G1 (December 2005)

--- arch/amd64_avx2+.sh
+++ arch/amd64_avx2+.sh
@@ -2,4 +2,4 @@
 ##arch/amd64_avx2.sh: Build definitions for amd64 with AVX2 support.
 ##                    Intel Haswell+, AMD bdver4+, VIA eden-x4+.
 ##@copyright GPL-2.0+
-CFLAGS_COMMON_ARCH=' -march=haswell -mno-rdrnd '
+CFLAGS_COMMON_ARCH=' -fomit-frame-pointer -march=haswell -mno-rdrnd '
